f8bbffcfca22792fb229a379318b59cc64258892
hokh <vladimirb0ndarev@yandex.ru>
2026-02-07 17:58:54 +0300

    add yandex file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 console.log("VITE CONFIG LOADED ✅");
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import generateRobotsTxt from "vite-plugin-robots-txt";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), generateRobotsTxt()]
 });
